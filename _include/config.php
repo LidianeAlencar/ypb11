8c46ec558c162dfde10f1477505a812d98925152
Lidiane <alencar.lidiane@gmail.com>
2016-07-19 13:37:56 -0300

    config

--- _include/config.php
+++ _include/config.php
@@ -1,7 +1,12 @@
 <?php
-//CONFIG PADRÃO DE URL
-$URL  = 'http://' . $_SERVER['HTTP_HOST'] . '/ybp11';
-$IMG  = 'http://' . $_SERVER['HTTP_HOST'] . '/ybp11/_public/img';
-$CSS  = 'http://' . $_SERVER['HTTP_HOST'] . '/ybp11/_public/css';
-$JS   = 'http://' . $_SERVER['HTTP_HOST'] . '/ybp11/_public/js';
-//echo $URL;
+//CONFIG PADRÃO DE URL DESENVOLVIMENTO
+// $URL  = 'http://' . $_SERVER['HTTP_HOST'] . '/ybp11';
+// $IMG  = 'http://' . $_SERVER['HTTP_HOST'] . '/ybp11/_public/img';
+// $CSS  = 'http://' . $_SERVER['HTTP_HOST'] . '/ybp11/_public/css';
+// $JS   = 'http://' . $_SERVER['HTTP_HOST'] . '/ybp11/_public/js';
+
+//CONFIG PADRÃO DE URL PRODUÇÃO
+$URL  = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+$IMG  = 'http://' . $_SERVER['HTTP_HOST'] . '/_public/img';
+$CSS  = 'http://' . $_SERVER['HTTP_HOST'] . '/_public/css';
+$JS   = 'http://' . $_SERVER['HTTP_HOST'] . '/_public/js';
